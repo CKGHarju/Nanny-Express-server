093fbfe75aaf6d0afa376be3a0eaaffc840c813f
jportella93 <jportella93@gmail.com>
2018-06-15 20:24:18 +0200

    feat: add nannies controller

--- controllers/nanny.controllers.js
+++ controllers/nanny.controllers.js
@@ -1,17 +1,18 @@
-const postNanny = async (ctx) => {
-  
-} 
+const User = require('../models/user.model');
 
-const putNanny = async (ctx) => {
-  
-} 
+//Receive an array of nannies from the user and update them.
+const putNannies = async (ctx) => {
+  const userId = ctx.user.id;
+  const nannies = ctx.body.nannies;
 
-const deleteNanny = async (ctx) => {
-  
-} 
+  User.findOneAndUpdate(
+    { _id: userId},
+    {nannies},
+    { new: true})
+  .then(res => ctx.response.body = res)
+  .catch(err => throw new Error (err.message))
+}
 
 module.exports = {
-  postNanny,
-  putNanny,
-  deleteNanny
+  putNannies,
 }
